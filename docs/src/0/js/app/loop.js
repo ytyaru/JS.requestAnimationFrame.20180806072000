7a67c57d232780a128d785fbfe8d8fcc80f4765c
ytyaru <ytyaru@outlook.jp>
2018-08-06 16:54:17 +0900

    バグ修正：onbeforeunloadの戻り値をセットした

--- docs/src/0/js/app/loop.js
+++ docs/src/0/js/app/loop.js
@@ -22,8 +22,7 @@ define(function() {
     C.Initialize = function() {
         C._Loop();
         C._requestId = window.requestAnimationFrame(C.Initialize); //戻り値を取得
-        window.onbeforeunload = function(e) { window.cancelAnimationFrame(C._requestId); alert("cancelAnimationFrame実行！"); }
-        return "cancelAnimationFrame実行！";
+        window.onbeforeunload = function(e) { window.cancelAnimationFrame(C._requestId); alert("cancelAnimationFrame実行！"); return "cancelAnimationFrame実行！"; }
     }
     return C;
 });
